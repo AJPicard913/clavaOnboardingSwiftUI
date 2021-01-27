dfa8ab3d247b3d76972f0553f9d6a2746c2a3705
AJ Picard <Contact@saucedesigns.io>
2021-01-27 08:37:36 -0500

    Initial Commit

---
+++ Shared/ClavaOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClavaOnboardingApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ClavaOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
